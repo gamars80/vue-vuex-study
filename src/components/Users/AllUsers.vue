707bf3507b3df5486a37fe5df3d7916358288a88
unknown <gamars80@gmail.com>
2019-05-20 17:21:42 +0900

    수정

--- src/components/Users/AllUsers.vue
+++ src/components/Users/AllUsers.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>All Users</h1>
+    <!--<h1>All Users({{ allUsersCount }})</h1>
+    <h3>Seoul Users : {{ countOfSeoul }}({{ percentOfSeoul }}%)</h3>-->
+    <h1>All Users({{ count }})</h1>
+    <h3>Seoul Users : {{ seouls }}({{ percent }}%)</h3>
     <v-list two-line>
       <v-list-tile 
         v-for="(user, index) in allUsers"
@@ -23,21 +26,34 @@
 
 <script>
 import { EventBus } from '@/main.js'
-
+import {mapState,mapGetters} from 'vuex'
   export default {
     data() {
       return {
-        allUsers:[
-          {userId: 'hoza123', password: '123', name: 'Hoza', address: 'Seoul', src:'https://goo.gl/oqLfJR'},
-          {userId: 'max123', password: '456', name: 'Max', address: 'Berlin', src:'https://goo.gl/Ksk9B9'},
-          {userId: 'lego123', password: '789', name: 'Lego', address: 'Busan', src:'https://goo.gl/x7SpCD'}
-        ]
+        //store에 복사
+        // allUsers:[
+        //   {userId: 'hoza123', password: '123', name: 'Hoza', address: 'Seoul', src:'https://goo.gl/oqLfJR'},
+        //   {userId: 'max123', password: '456', name: 'Max', address: 'Berlin', src:'https://goo.gl/Ksk9B9'},
+        //   {userId: 'lego123', password: '789', name: 'Lego', address: 'Busan', src:'https://goo.gl/x7SpCD'}
+        // ]
       }
+    },
+    computed:{
+      ...mapState(['allUsers']),
+      ...mapGetters({
+        count : 'allUsersCount',
+        seouls : 'countOfSeoul',
+        percent : 'percentOfSeoul'
+
+      })
+      //...mapGetters(['allUsersCount','countOfSeoul','percentOfSeoul'])
     },
     mounted() {
       EventBus.$on('signUp', users => {
-        this.allUsers.push(users)
+        //this.allUsers.push(users)
+        this.$store.state.allUsers.push(users)
       })
     }
   }
 </script>
+ 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <h4>모든 유저의수 : {{$store.getters.allUsersCount}}</h4>
+
   </v-container>
 </template>
